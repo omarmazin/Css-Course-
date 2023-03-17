a5a155ee3dc1502fee4589132584549aeaeb3267
omarmazin <omr.ad@outlook.com>
2023-03-17 14:38:11 -0700

    1 Week

---
+++ 1 Week/main.js
@@ -0,0 +1,21 @@
+const circle = {
+  radius: 1,
+  locaton: {
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    console.log("draw");
+  },
+};
+const circle = {
+    radius: 1,
+    locaton: {
+      x: 1,
+      y: 1,
+    },
+    draw: function () {
+      console.log("draw");
+    },
+  };
+circle.draw()
